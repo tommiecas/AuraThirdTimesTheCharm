18f1ba65e9587c20a0d28812c2c2de1a8caaffa4
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-08-20 18:52:07 -0700

    Enemy Interface

--- Source/Aura/Private/Characters/AuraEnemy.cpp
+++ Source/Aura/Private/Characters/AuraEnemy.cpp
@@ -3,3 +3,12 @@
 
 #include "Characters/AuraEnemy.h"
 
+void AAuraEnemy::HighlightActor()
+{
+	
+}
+
+void AAuraEnemy::UnHighlightActor()
+{
+	
+}
--- Source/Aura/Public/Characters/AuraEnemy.h
+++ Source/Aura/Public/Characters/AuraEnemy.h
@@ -4,14 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "Characters/AuraCharacterBase.h"
+#include "Interactions/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacterBase
+class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
+
+public:
+	virtual void HighlightActor() override;
+	virtual void UnHighlightActor() override;
 	
 };
