6978490fa7efcb45c0ac4e3a371264e61448c0d6
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-08-21 13:29:05 -0700

    Duration Gameplay Effects

--- Source/Aura/Public/Actors/AuraEffectActor.h
+++ Source/Aura/Public/Actors/AuraEffectActor.h
@@ -26,6 +26,9 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+
 private:
 
 
