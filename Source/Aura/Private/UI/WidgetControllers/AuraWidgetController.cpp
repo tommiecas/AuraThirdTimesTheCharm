0578ad3b274e76aa416135dc78b1956d4eb10e77
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-08-21 00:59:46 -0700

    Overlay Widget Controller

--- Source/Aura/Private/UI/WidgetControllers/AuraWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetControllers/AuraWidgetController.cpp
@@ -3,3 +3,10 @@
 
 #include "UI/WidgetControllers/AuraWidgetController.h"
 
+void UAuraWidgetController::SetWidgetControllerParams(const FWidgetControllerParams& WCParams)
+{
+	PlayerController = WCParams.PlayerController;
+	PlayerState = WCParams.PlayerState;
+	AbilitySystemComponent = WCParams.AbilitySystemComponent;
+	AttributeSet = WCParams.AttributeSet;
+}
---
+++ Source/Aura/Public/UI/WidgetControllers/OverlayWidgetController.h
@@ -0,0 +1,17 @@
+// Copyright Gravity Well Games
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetControllers/AuraWidgetController.h"
+#include "OverlayWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UOverlayWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
