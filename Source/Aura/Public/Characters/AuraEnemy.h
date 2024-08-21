5b950ac60ca629714d457d1866908c0e85c32303
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-08-20 21:02:42 -0700

    Post Process Highlight

--- Source/Aura/Public/Characters/AuraEnemy.h
+++ Source/Aura/Public/Characters/AuraEnemy.h
@@ -16,10 +16,12 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 
 public:
+	AAuraEnemy();
+
+public:
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
 
-	UPROPERTY(BlueprintReadOnly)
-	bool bHighlighted = false;
+
 	
 };
