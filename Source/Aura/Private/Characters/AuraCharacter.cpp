1f643882d729b86afe00a53fefae510099aa1dae
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-08-23 15:46:31 -0700

    Derived Attributes

--- Source/Aura/Private/Characters/AuraCharacter.cpp
+++ Source/Aura/Private/Characters/AuraCharacter.cpp
@@ -56,5 +56,5 @@ void AAuraCharacter::InitAbilityActorInfo()
 			AuraHUD->InitOverlay(AuraPlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
 		}
 	}
-	InitializePrimaryAttributes();
+	InitializeDefaultAttributes();
 }
