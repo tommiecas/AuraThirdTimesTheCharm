8237490e6c37aed1a9adb2b573aafb1c304e12ee
tommiecas <157588974+tommiecas@users.noreply.github.com>
2024-08-26 06:25:54 -0700

    Projectile Impact

--- Source/Aura/Public/Actors/AuraProjectile.h
+++ Source/Aura/Public/Actors/AuraProjectile.h
@@ -3,10 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "../../../../../../../../../Program Files/Epic Games/UE_5.4/Engine/Plugins/FX/Niagara/Source/Niagara/Classes/NiagaraSystem.h"
 #include "GameFramework/Actor.h"
 #include "AuraProjectile.generated.h"
 
 class USphereComponent;
+class UAudioComponent;
 class UProjectileMovementComponent;
 
 UCLASS()
@@ -21,12 +23,29 @@ public:
 	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
 protected:
 	virtual void BeginPlay() override;
+	virtual void Destroyed() override;
 
 	UFUNCTION()
 	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	bool bHit = false;
 private:
 
 	UPROPERTY(VisibleAnywhere)
 	TObjectPtr<USphereComponent> Sphere;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UNiagaraSystem> ImpactEffect;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USoundBase> ImpactSound;
+
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<USoundBase> LoopingSound;
+
+	UPROPERTY()
+	TObjectPtr<UAudioComponent> LoopingSoundComponent;
+
+	UPROPERTY(EditDefaultsOnly)
+	float LifeSpan = 15.f;
 };
